0f56f4e9904925f54b14e60eae80ef830edd63c0
Roberto Bustamante <rabustamante5@utpl.edu.ec>
2022-10-28 12:42:50 -0500

    parte del trabajo en clase

---
+++ Ejercicio05/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author UTPL
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        int var1 = 2;
+        int var2 = 2;
+        int var3 = 12;
+        int var4 = 1;
+        int var5 = 3;
+        double resultado = (Math.pow(var2, var1) + 12 - 1 / 3);
+        System.out.println(resultado);
+    }
+}
